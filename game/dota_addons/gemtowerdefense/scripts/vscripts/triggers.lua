a8837af86f7a5431a1d9a645f988532e7ae823bd
Krešimir Mađerić <Kreimir Maeri>
2016-12-24 19:03:43 +0100

    Units don't get destroyed anymore when entering baseunit:Destroy(), rather get killed unit:ForceKill().

--- game/dota_addons/gemtowerdefense/scripts/vscripts/triggers.lua
+++ game/dota_addons/gemtowerdefense/scripts/vscripts/triggers.lua
@@ -1,40 +1,16 @@
 function OnTouchGemCastle(trigger)
 
-	print("Triggered")
-
 	local unit = trigger.activator
 	local eHandle = unit:GetEntityHandle()
 
 	Rounds:DecrementBaseHealth(unit.Damage)
 	Rounds:IncrementTotalLeaked()
 
+	CustomNetTables:SetTableValue( "game_state", "gem_castle_health", { value = Rounds:GetBaseHealth() } )
 
-	--print("Health Point", self.BaseHealth)
-
-	CustomNetTables:SetTableValue( "game_state", "gem_castle_health", { Rounds:GetBaseHealth() } )
-
-
-	unit:Destroy()
+	unit:ForceKill(false)
     Rounds:DeleteByHandle(eHandle)
 	
-    
-    if Rounds:GetBaseHealth() <= 0 then
-
-		GameRules:SetGameWinner(DOTA_TEAM_BADGUYS)
-
-	end
-
-	
-    Rounds:IncrementKillNumber()
-
-	if Rounds:GetAmountOfKilled() == 10 then
-
-
-		Rounds:IncrementRound()
-		Rounds:ResetKillNumber()
-		Rounds:Build()
-
-	end
-
 
 end
+
