9e21c61c094d8ce640ef3e25209232acf5f2dec4
DNLHC <machineiscoming@gmail.com>
2017-01-05 17:24:28 +0300

    Added players states for hero selection

--- content/dota_addons/gemtowerdefense/panorama/scripts/custom_game/hero_selection.js
+++ content/dota_addons/gemtowerdefense/panorama/scripts/custom_game/hero_selection.js
@@ -22,10 +22,30 @@ function pickHero() {
 
   GameEvents.SendCustomGameEventToServer("player_selected_hero", {selected_hero : 'npc_dota_hero_' + selectedHero});
   
-  $.GetContextPanel().DeleteAsync(0);
+}
+
+function endHeroSelection() {
+  var Root = $.GetContextPanel();
+  Root.style.opacity = 0;
+  $.Schedule(1.5, function() {
+    Root.DeleteAsync(0);
+  });
 }
 
 
 (function(){
   $("#pick-hero-button").disabled = true;
+  
+  var players = Game.GetAllPlayerIDs();
+  var panel = $("#players-states").FindChildrenWithClassTraverse('players-states-item');
+
+  for (var i = 0; i < players.length; i++) {
+    var playerInfo = Game.GetPlayerInfo(i);
+    var target = panel[i];
+
+    for (var j = 0; j < target.GetChildCount(); j++) {
+      target.GetChild(j).steamid = playerInfo.player_steamid;
+      target.style.visibility = 'visible';
+    }
+	}
 })();
