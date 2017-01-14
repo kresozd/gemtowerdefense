484cc008d8409071aa06296d0396eb29dfa26e27
DNLHC <machineiscoming@gmail.com>
2017-01-14 07:36:59 +0300

    added layout snippets for rank players and a temporar function for filling leaderboard with random steam users, minor color changes

---
+++ content/dota_addons/gemtowerdefense/panorama/scripts/custom_game/info_tabs/leaderboard_tab.js
@@ -0,0 +1,61 @@
+var activeTab = 0;
+var tabs = $('#leaderboard-tabs').Children();
+var buttons = $('#leaderboard-nav').FindChildrenWithClassTraverse('leaderboard-button');
+var leaderboards = ['one-player', 'two-player', 'three-player', 'four-player'];
+
+function activateLeaderboardTab(num) {
+  if (num !== activeTab) {
+    tabs[activeTab].RemoveClass("leaderboard-tab-activated");
+    tabs[num].AddClass("leaderboard-tab-activated");
+
+    buttons[num].AddClass("leaderboard-button-active");
+    buttons[activeTab].RemoveClass("leaderboard-button-active");
+
+    activeTab = num;
+  }
+}
+
+// temporary just for showcase
+function addLeaderboardWinners() {
+  var localInfo = Game.GetPlayerInfo(Game.GetLocalPlayerID());
+  var localSteam = localInfo.player_steamid;
+
+  for (var name of leaderboards) {
+
+    var leaderboardPanel = $('#leaderboard-' + name).GetChild(0);
+
+    for (var i = 1; i <= 10; i++) {
+
+      var leaderboardItem = $.CreatePanel('Panel', leaderboardPanel, '');
+      leaderboardItem.BLoadLayoutSnippet(name + '-item');
+
+      leaderboardItem.FindChildTraverse('leaderboard-place-value').text = i + '.';
+      var players = leaderboardItem.FindChildTraverse('leaderboard-players'); 
+
+      for (var j = 0; j < players.GetChildCount(); j++) {
+
+        if (name == 'one-player') {
+          var steam = randomSteam(localSteam);
+          players.GetChild(0).steamid = steam;
+          players.GetChild(1).steamid = steam;
+          continue;
+        }
+
+        players.GetChild(j).steamid = randomSteam(localSteam);
+      }
+    }
+  }
+}
+
+
+function randomSteam(steam) {
+  var random = Math.floor(Math.random() * (99 - 10 + 1)) + 10;
+  result = steam.slice(0, -2) + random;
+  
+  return result;
+}
+
+
+(function() {
+  addLeaderboardWinners();
+})();
