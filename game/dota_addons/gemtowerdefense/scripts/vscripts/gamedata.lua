877525712f1b200b950af474f5e5cc831e5ede35
Krešimir Mađerić <Kreimir Maeri>
2017-02-22 17:11:22 +0100

    Fixed typo.

--- game/dota_addons/gemtowerdefense/scripts/vscripts/gamedata.lua
+++ game/dota_addons/gemtowerdefense/scripts/vscripts/gamedata.lua
@@ -8,7 +8,7 @@ function GameData:Init()
 
    ListenToGameEvent("throne_touch",Dynamic_Wrap(GameData, 'OnLeaked'), self)
    ListenToGameEvent("entity_killed", Dynamic_Wrap(GameData, 'OnEntityKilled'), self)
-   ListenToGameEvent("entity_hurt"), Dynamic_Wrap(GameData, 'OnEntityHurt'), self)
+   ListenToGameEvent("entity_hurt", Dynamic_Wrap(GameData, 'OnEntityHurt'), self)
 
     self.LeakCount = {}
     self.Killed = 0
@@ -26,7 +26,7 @@ end
 
 
 function GameData:OnEntityHurt(keys)
-{
-    CustomGameEventManager:Send_ServerToAllClients( "update_tower_stats_damage", {damageTable = Wave.TowerDamage} )
 
-}
+    CustomGameEventManager:Send_ServerToAllClients( "update_tower_stats_damage", {damageTable = Wave.TowerDamage} )
+    print("Entity hurt!")
+end
