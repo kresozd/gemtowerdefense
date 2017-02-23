26df2098f39edbdeebd7d56740fc75df74b21191
PhysicsGuy <physics6.63@gmail.com>
2017-02-22 18:15:32 -0700

    Tower damage data for panorama

--- game/dota_addons/gemtowerdefense/scripts/vscripts/gamedata.lua
+++ game/dota_addons/gemtowerdefense/scripts/vscripts/gamedata.lua
@@ -13,6 +13,7 @@ function GameData:Init()
     self.LeakCount = {}
     self.Killed = 0
     self.Round  = 0
+    self.DamageFromTowers = {}
 end
 
 
@@ -27,6 +28,41 @@ end
 
 function GameData:OnEntityHurt(keys)
 
-    CustomGameEventManager:Send_ServerToAllClients( "update_tower_stats_damage", {damageTable = Wave.TowerDamage} )
-    print("Entity hurt!")
+local allDamage = Wave.TowerDamage
+local topFiveTowers = {
+  [1]={},
+  [2]={},
+  [3]={},
+  [4]={},
+  [5]={}
+}
+
+local maxDamage={
+  [1]=0,
+  [2]=0,
+  [3]=0,
+  [4]=0,
+  [5]=0
+}
+for i,j in pairs(allDamage) do
+  if j>=maxDamage[1] then
+    topFiveTowers[1][i]=j
+    maxDamage[1]=j
+  elseif j>maxDamage[2] then
+    topFiveTowers[2][i]=j
+    maxDamage[2]=j
+  elseif j>maxDamage[3] then
+    topFiveTowers[3][i]=j
+    maxDamage[3]=j
+  elseif j>maxDamage[4] then
+    topFiveTowers[4][i]=j
+    maxDamage[4]=j
+  elseif j>maxDamage[5] then
+    topFiveTowers[5][i]=j
+    maxDamage[5]=j        
+  end
+end
+
+    CustomGameEventManager:Send_ServerToAllClients( "update_tower_stats_damage", {damageTable = topFiveTowers} )
+    --print("Entity hurt!")
 end
