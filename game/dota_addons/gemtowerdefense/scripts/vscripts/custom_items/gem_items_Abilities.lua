d49d5c6641e733d63228e5e4e4a567065f0c0eea
DNLHC <machineiscoming@gmail.com>
2017-02-27 03:48:41 +0300

    Please don't repeat yourself

--- game/dota_addons/gemtowerdefense/scripts/vscripts/custom_items/gem_items_Abilities.lua
+++ game/dota_addons/gemtowerdefense/scripts/vscripts/custom_items/gem_items_Abilities.lua
@@ -1,36 +1,10 @@
+function AbilityAdd(keys)
+	local gem = keys.Gem
+	local chance = Random.TowerTypeChance[gem]
+	chance = chance + 1
+	print("Success of "..gem.." Ability",  chance)
+end
 
-function AbilityAddamethyst(keys)
-	Random.TowerTypeChance["Amethyst"]=Random.TowerTypeChance["Amethyst"]+1
-	print("Success of Amethyst Ability",Random.TowerTypeChance["Amethyst"])
-end
-function AbilityAdddiamond(keys)
-	Random.TowerTypeChance["Diamond"]=Random.TowerTypeChance["Diamond"]+1
-	print("Success of Diamond Ability",Random.TowerTypeChance["Diamond"])
-end
-function AbilityAddemerald(keys)
-	Random.TowerTypeChance["Emerald"]=Random.TowerTypeChance["Emerald"]+1
-	print("Success of Emerald Ability",Random.TowerTypeChance["Emerald"])
-end
-function AbilityAddopal(keys)
-	Random.TowerTypeChance["Opal"]=Random.TowerTypeChance["Opal"]+1
-	print("Success of Opal Ability",Random.TowerTypeChance["Opal"])
-end
-function AbilityAddaquamarine(keys)
-	Random.TowerTypeChance["Aquamarine"]=Random.TowerTypeChance["Aquamarine"]+1
-	print("Success of Aquamarine Ability",Random.TowerTypeChance["Aquamarine"])
-end
-function AbilityAddruby(keys)
-	Random.TowerTypeChance["Ruby"]=Random.TowerTypeChance["Ruby"]+1
-	print("Success of Ruby Ability",Random.TowerTypeChance["Ruby"])
-end
-function AbilityAddsapphire(keys)
-	Random.TowerTypeChance["Sapphire"]=Random.TowerTypeChance["Sapphire"]+1
-	print("Success of Sapphire Ability",Random.TowerTypeChance["Sapphire"])
-end
-function AbilityAddtopaz(keys)
-	Random.TowerTypeChance["Topaz"]=Random.TowerTypeChance["Topaz"]+1
-	print("Success of Topaz Ability",Random.TowerTypeChance["Topaz"])
-end
 function AbilityHealThrone(keys)
 	local healVal = RandomInt(3,8)
 	Throne:HealThrone(healVal)
