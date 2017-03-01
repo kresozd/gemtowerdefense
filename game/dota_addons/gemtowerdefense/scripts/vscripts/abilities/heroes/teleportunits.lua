59eef871aad0251b75878ec662cfffcade7da5d1
Krešimir Mađerić <Kreimir Maeri>
2017-03-01 13:03:26 +0100

    Tried to make particles for this spell. Not working yet.

--- game/dota_addons/gemtowerdefense/scripts/vscripts/abilities/heroes/teleportunits.lua
+++ game/dota_addons/gemtowerdefense/scripts/vscripts/abilities/heroes/teleportunits.lua
@@ -4,9 +4,12 @@ function Teleport(keys)
     local units = Wave:GetEnemies()
     local vectorMap = Grid:GetVectorMap()
     --local mapSize = table.getn(vectorMap)
+   -- local particleVFX = "particles/units/heroes/heroes_underlord/abyssal_underlord_darkrift_target.vpcf"
+    
     for key, unit in pairs(units) do
         local currentStep = unit.Step
-        unit.Step = RandomInt(unit.Step - DISTANCE , unit.Step)
+       -- local particle = ParticleManager:CreateParticle(particleVFX, PATTACH_ABSORIGIN, handle_3)
+        unit.Step = RandomInt(unit.Step , unit.Step + 5)
         unit:SetAbsOrigin(vectorMap[unit.Step])
     end
 
